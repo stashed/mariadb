c6aada1bdf2cec16fc4524d1e6124e84e2f0190f
Hossain Mahmud <hmsayem@appscode.com>
2022-01-20 15:05:55 +0600

    Add support for backup and restore from different namespace (#161)
    /cherry-pick
    Signed-off-by: hmsayem <hmsayem@appscode.com>

--- pkg/utils.go
+++ pkg/utils.go
@@ -17,17 +17,22 @@ limitations under the License.
 package pkg
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"path/filepath"
+	"strings"
 
 	stash "stash.appscode.dev/apimachinery/client/clientset/versioned"
 	"stash.appscode.dev/apimachinery/pkg/restic"
 
-	"gomodules.xyz/go-sh"
-	core "k8s.io/api/core/v1"
+	shell "gomodules.xyz/go-sh"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
-	"kmodules.xyz/custom-resources/apis/appcatalog/v1alpha1"
+	kmapi "kmodules.xyz/client-go/api/v1"
+	appcatalog "kmodules.xyz/custom-resources/apis/appcatalog/v1alpha1"
 	appcatalog_cs "kmodules.xyz/custom-resources/client/clientset/versioned"
 )
 
@@ -51,28 +56,84 @@ type mariadbOptions struct {
 	myArgs            string
 	waitTimeout       int32
 	outputDir         string
+	storageSecret     kmapi.ObjectReference
 
 	setupOptions  restic.SetupOptions
 	backupOptions restic.BackupOptions
 	dumpOptions   restic.DumpOptions
 }
 
-func (opt *mariadbOptions) waitForDBReady(appBinding *v1alpha1.AppBinding, secret *core.Secret) error {
-	klog.Infoln("Waiting for the database to be ready.....")
-	shell := sh.NewSession()
-	shell.SetEnv(EnvMariaDBPassword, string(secret.Data[MariaDBPassword]))
-	args := []interface{}{
-		"ping",
-		"--host", appBinding.Spec.ClientConfig.Service.Name,
-		"--user", string(secret.Data[MariaDBUser]),
+type sessionWrapper struct {
+	sh  *shell.Session
+	cmd *restic.Command
+}
+
+func (opt *mariadbOptions) newSessionWrapper(cmd string) *sessionWrapper {
+	return &sessionWrapper{
+		sh: shell.NewSession(),
+		cmd: &restic.Command{
+			Name: cmd,
+		},
 	}
-	if appBinding.Spec.ClientConfig.Service.Port != 0 {
-		args = append(args, fmt.Sprintf("--port=%d", appBinding.Spec.ClientConfig.Service.Port))
+}
+
+func (session *sessionWrapper) setDatabaseCredentials(kubeClient kubernetes.Interface, appBinding *appcatalog.AppBinding) error {
+	appBindingSecret, err := kubeClient.CoreV1().Secrets(appBinding.Namespace).Get(context.TODO(), appBinding.Spec.Secret.Name, metav1.GetOptions{})
+	if err != nil {
+		return err
 	}
 
+	err = appBinding.TransformSecret(kubeClient, appBindingSecret.Data)
+	if err != nil {
+		return err
+	}
+
+	session.cmd.Args = append(session.cmd.Args, "-u", string(appBindingSecret.Data[MariaDBUser]))
+	session.sh.SetEnv(EnvMariaDBPassword, string(appBindingSecret.Data[MariaDBPassword]))
+	return nil
+}
+
+func (session *sessionWrapper) setDatabaseConnectionParameters(appBinding *appcatalog.AppBinding) error {
+	hostname, err := appBinding.Hostname()
+	if err != nil {
+		return err
+	}
+	session.cmd.Args = append(session.cmd.Args, "-h", hostname)
+
+	port, err := appBinding.Port()
+	if err != nil {
+		return err
+	}
+	// if port is specified, append port in the arguments
+	if port != 0 {
+		session.cmd.Args = append(session.cmd.Args, fmt.Sprintf("--port=%d", port))
+	}
+	return nil
+}
+
+func (session *sessionWrapper) setUserArgs(args string) {
+	for _, arg := range strings.Fields(args) {
+		session.cmd.Args = append(session.cmd.Args, arg)
+	}
+}
+
+func (session *sessionWrapper) setTLSParameters(appBinding *appcatalog.AppBinding, scratchDir string) error {
+	// if ssl enabled, add ca.crt in the arguments
 	if appBinding.Spec.ClientConfig.CABundle != nil {
-		args = append(args, fmt.Sprintf("--ssl-ca=%v", filepath.Join(opt.setupOptions.ScratchDir, MariaDBTLSRootCA)))
-	}
+		if err := ioutil.WriteFile(filepath.Join(scratchDir, MariaDBTLSRootCA), appBinding.Spec.ClientConfig.CABundle, os.ModePerm); err != nil {
+			return err
+		}
+		tlsCreds := []interface{}{
+			fmt.Sprintf("--ssl-ca=%v", filepath.Join(scratchDir, MariaDBTLSRootCA)),
+		}
 
-	return shell.Command("mysqladmin", args...).Run()
+		session.cmd.Args = append(session.cmd.Args, tlsCreds...)
+	}
+	return nil
+}
+
+func (session *sessionWrapper) waitForDBReady() error {
+	klog.Infoln("Waiting for the database to be ready....")
+	args := append(session.cmd.Args, "ping")
+	return session.sh.Command("mysqladmin", args...).Run()
 }
